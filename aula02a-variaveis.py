050b6d90fac5c7176b2cf99351857a97c13c02dd
TiMuhlmann14 <tiagomuhlmann@gmail.com>
2026-03-18 20:48:38 -0300

    Aula 02 Variáveis

---
+++ aula02a-variaveis.py
@@ -0,0 +1,15 @@
+# Desafio 1
+'''nome = input(" Digite seu nome: ")
+print(f"Bem vindo, {nome}")
+
+
+# Desafio 2
+print("DATA DE NASCIMENTO")
+dia = input("Dia: ")
+mes = input("Mês: ")
+ano = input("Ano: ")
+print (f"{dia}/{mes}/{ano}")
+'''
+
+#Desafio 3
+num1 = int(input(" "))
